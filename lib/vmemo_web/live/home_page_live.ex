ba8e12bae7bd90b838abc61629654b5ab73b9358
TJ <thaddeusjiang@gmail.com>
2025-11-06 13:56:05 +0900

    ui

--- lib/vmemo_web/live/home_page_live.ex
+++ lib/vmemo_web/live/home_page_live.ex
@@ -19,7 +19,7 @@ defmodule VmemoWeb.HomePageLive do
     <section class="pt-12 px-4 pb-4 sm:pt-16 sm:px-4 sm:pb-4 lg:pt-20 lg:px-4 lg:pb-4 grow relative">
       <div class="flex flex-col items-center justify-center h-full gap-8">
         <div class="flex flex-col items-center gap-6 w-full max-w-md px-4">
-          <img src={~p"/images/logo.svg"} class="h-24 w-24" alt="Vmemo Logo" />
+          <h1 class="text-4xl font-bold">Search</h1>
 
           <.live_component
             module={SearchBox}
