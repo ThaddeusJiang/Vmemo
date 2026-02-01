42e2e6c7e95de933fb6bcfc2029a778de8ecd6e0
Amami <thaddeusjiang@gmail.com>
2026-02-02 08:04:03 +0900

    wip

--- lib/vmemo/admin/import_request.ex
+++ lib/vmemo/admin/import_request.ex
@@ -11,6 +11,14 @@ defmodule Vmemo.Admin.ImportRequest do
 
   admin do
     table_columns([:id, :status, :source_filename, :inserted_at, :updated_at])
+    create_actions([:import])
+
+    form do
+      field :import_zip do
+        max_file_size 1024 * 1024 * 1024
+        accepted_extensions [".zip", "application/zip"]
+      end
+    end
   end
 
   code_interface do
@@ -26,6 +34,50 @@ defmodule Vmemo.Admin.ImportRequest do
     create :create do
       accept [:source_filename, :metadata]
       change set_attribute(:status, "pending")
+    end
+
+    create :import do
+      accept [:metadata]
+      argument :import_zip, Ash.Type.File, allow_nil?: false
+      change set_attribute(:status, "pending")
+      require_atomic? false
+
+      change fn changeset, _context ->
+        with %Ash.Type.File{} = file <- Ash.Changeset.get_argument(changeset, :import_zip),
+             {:ok, path} <- Ash.Type.File.path(file) do
+          filename = Path.basename(path)
+          dest_dir = Path.join(System.tmp_dir!(), "vmemo-import-upload")
+          File.mkdir_p!(dest_dir)
+          dest_path = Path.join(dest_dir, "#{System.unique_integer([:positive])}-#{filename}")
+          File.cp!(path, dest_path)
+
+          changeset
+          |> Ash.Changeset.change_attribute(:source_filename, filename)
+          |> Ash.Changeset.put_context(:import_zip_path, dest_path)
+        else
+          _ ->
+            Ash.Changeset.add_error(changeset,
+              field: :import_zip,
+              message: "Failed to read uploaded ZIP file"
+            )
+        end
+      end
+
+      change after_action(fn changeset, record, _context ->
+               case changeset.context[:import_zip_path] do
+                 nil ->
+                   {:ok, record}
+
+                 zip_path ->
+                   %{request_id: record.id, zip_path: zip_path}
+                   |> Vmemo.Workers.ProcessImportRequest.new()
+                   |> Oban.insert()
+                   |> case do
+                     {:ok, _job} -> {:ok, record}
+                     {:error, error} -> {:error, error}
+                   end
+               end
+             end)
     end
 
     update :update do
