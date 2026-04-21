cad3a85b53a0f40791a459223aa64e385b792891
天海 原 / AMAMI Hara <thaddeusjiang@gmail.com>
2026-04-21 22:31:29 +0900

    chore(config): 统一 dev/test URL 环境变量并新增 worktree skill (#143)

--- config/dev.exs
+++ config/dev.exs
@@ -2,18 +2,19 @@ import Config
 
 config :ash, policies: [show_policy_breakdowns?: true]
 
+dev_database_url =
+  System.fetch_env!("DEV_DATABASE_URL")
+
+dev_typesense_url = System.fetch_env!("DEV_TYPESENSE_URL")
+
 # Configure your database
 config :vmemo, Vmemo.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "vmemo_dev",
-  port: 15432,
+  url: dev_database_url,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
-config :vmemo, typesense_url: "http://localhost:18108"
+config :vmemo, typesense_url: dev_typesense_url
 config :vmemo, typesense_api_key: "xyz"
 
 config :vmemo, moondream_url: "http://localhost:2020/v1/"
